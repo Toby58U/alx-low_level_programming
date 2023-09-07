9a0e224b8b53368650159d0236fe7dcff5edab25
Toby58U <tobennaumeh001@gmail.com>
2023-09-07 09:19:34 -0700

    Hello world commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints a line with the puts function
+ * main - Entry point
  *
- * Return value: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
