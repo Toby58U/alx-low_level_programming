350e3f839dd915c9202ead96a0069fdbd7765db2
Toby58U <tobennaumeh001@gmail.com>
2023-10-03 01:50:39 -0700

    Malloc and free commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * *create_array creates an array of chars,
+ * *create_array - creates an array of chars,
  * and initializes it with a specific char
  * @size: is the size of array to create
  * @c: is the char to initialize the array c
