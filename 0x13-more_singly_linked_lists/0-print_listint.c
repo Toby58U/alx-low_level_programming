91d9fc2ed32625ac42ab2f03a4980bc34894557d
Toby58U <tobennaumeh001@gmail.com>
2023-10-23 19:15:06 -0700

    More singly linked lists commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t node_count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		num++;
 		h = h->next;
+		node_count++;
 	}
 
-	return (num);
+	return (node_count);
 }
