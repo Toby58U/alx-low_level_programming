67baf97edfa72a79ff6215d9966ab0e639ba4e19
Toby58U <tobennaumeh001@gmail.com>
2023-12-07 13:26:33 -0800

    Doubly linked lists commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,35 +11,31 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *current;
-	unsigned int i;
+	dlistint_t *new_node, *current = *h;
 
-	if (h == NULL)
+	if (!h || (idx != 0 && !current))
 		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
 
 	if (idx == 0)
 	{
+		new_node->next = current;
+		if (current)
+			current->prev = new_node;
 		new_node->prev = NULL;
-		new_node->next = *h;
-		if (*h != NULL)
-			(*h)->prev = new_node;
-			*h = new_node;
+		*h = new_node;
 		return (new_node);
 	}
 
-	current = *h;
-	for (i = 0; i < idx - 1 && current != NULL; i++)
-	{
+	for (; idx > 1 && current; idx--)
 		current = current->next;
-	}
 
-	if (current == NULL)
+	if (!current)
 	{
 		free(new_node);
 		return (NULL);
@@ -48,7 +44,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = current;
 	new_node->next = current->next;
 
-	if (current->next != NULL)
+	if (current->next)
 		current->next->prev = new_node;
 
 	current->next = new_node;
