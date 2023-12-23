37f28d5a77f48acf4cd899322c9c63589b4b14b6
Toby58U <tobennaumeh001@gmail.com>
2023-12-22 19:03:04 -0800

    Hash tables commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,23 +9,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *ht;
 	unsigned long int i;
 
-	/* Allocate memory for the hash table structure */
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
-		return (NULL);	/* Unable to allocate memory for the hash table */
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-	/* Allocate memory for the array of hash nodes */
-	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_table->array == NULL)
-		return (NULL);	/* Unable to allocate memory for the array */
-
-	/* Initialize each element in the array to NULL */
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
-		new_table->array[i] = NULL;
+		ht->array[i] = NULL;
 
-	return (new_table);
+	return (ht);
 }
